623f6c860dc7b801a3b5fb4bbb40b96272e21f66
Dan7nilo <dan6nilo@gmail.com>
2023-02-12 13:11:45 -0300

    TESTE
    Incrementando animações de inicialização de página

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     alert("teste")
 })
+
+$('#seta').click(function(){
+    e.preventDefault();
+    alert("sfdaf");
+});
+
+$(function(){
+    $("seta").on("click",function(){
+        alert("asddfasf")
+    })
+})
